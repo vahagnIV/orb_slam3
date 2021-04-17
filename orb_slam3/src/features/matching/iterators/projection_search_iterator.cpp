01945bc88c80540e57df7beb8be6a62f686b9ea4
vahagnIV <vahagn.yeghikyan@gmail.com>
2021-04-17 10:04:33 +0400

    Added motion model tracking

--- orb_slam3/src/features/matching/iterators/projection_search_iterator.cpp
+++ orb_slam3/src/features/matching/iterators/projection_search_iterator.cpp
@@ -11,11 +11,11 @@ namespace iterators {
 
 ProjectionSearchIterator::ProjectionSearchIterator(std::unordered_set<map::MapPoint *>::iterator begin,
                                                    std::unordered_set<map::MapPoint *>::iterator end,
-                                                   const std::unordered_set<map::MapPoint *> *to_map_points,
-                                                   const Features *from_features,
-                                                   const geometry::Pose *pose,
-                                                   const camera::MonocularCamera *camera,
-                                                   const IFeatureExtractor *feature_extractor)
+                                                   const std::unordered_set<map::MapPoint *> * to_map_points,
+                                                   const Features * from_features,
+                                                   const geometry::Pose * pose,
+                                                   const camera::MonocularCamera * camera,
+                                                   const IFeatureExtractor * feature_extractor)
     : it_(begin),
       end_(end),
       to_map_points_(to_map_points),
@@ -32,9 +32,10 @@ ProjectionSearchIterator & ProjectionSearchIterator::operator++() {
 }
 
 void ProjectionSearchIterator::AdvanceIteratorUntilGood() {
-  while (it_ != end_ && (to_map_points_->find(*it_) != to_map_points_->end() && !pointee_.SetMapPointAndCompute(*it_,
-                                                                                                                camera_,
-                                                                                                                pose_)))
+  while (it_ != end_ && (to_map_points_->find(*it_) != to_map_points_->end() &&
+      !pointee_.SetMapPointAndCompute(*it_,
+                                      camera_,
+                                      pose_)))
     ++it_;
   if (it_ != end_)
     pointee_.InitializeIterators();
