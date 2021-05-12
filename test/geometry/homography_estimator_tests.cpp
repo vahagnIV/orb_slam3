c3a951521098a7bacebcf032926299781c0b4dd3
kterjanyan <karen@nvision.am>
2021-05-12 17:48:17 +0400

    - Refactor main.cpp - Implemented live camera

--- test/geometry/homography_estimator_tests.cpp
+++ test/geometry/homography_estimator_tests.cpp
@@ -52,14 +52,13 @@ TEST_F(HomographyEstimatorTests, HomographyEstimatorWorksCorrectly) {
     ASSERT_TRUE(geometry::utils::ComputeReprojectionError(H.inverse() * points_to[i], points_from[i]) < 1e-12);
   }
 
-  std::unordered_set<std::size_t> inliers;
   std::unordered_map<size_t, TPoint3D> triangulated;
   geometry::Pose pose;
   for (int i = 0; i < points_to.size(); ++i) {
     points_to[i] /= points_to[i][2];
     points_from[i] /= points_from[i][2];
   }
-  estimator.FindPose(H, points_to, points_from, matches, inliers, triangulated, pose);
+  estimator.FindPose(H, points_to, points_from, matches, triangulated, pose);
   ASSERT_TRUE((pose.T.normalized() - T.normalized()).norm() < 1e-12);
   ASSERT_TRUE((pose.R - R).norm() < 1e-12);
 }
