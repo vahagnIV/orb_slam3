4c5b6e1fa4027b59df61b30fa751448d1f38210e
vahagnIV <vahagn.yeghikyan@gmail.com>
2021-11-11 16:11:10 +0400

    Started implementing ros support

---
+++ ros/src/odometry_publisher/src/odometry_publisher.h
@@ -0,0 +1,22 @@
+//
+// Created by vahagn on 11/11/2021.
+//
+
+#ifndef ORB_SLAM3_ROS_SRC_ODOMETRY_PUBLISHER_SRC_ODOMETRY_PUBLISHER_H_
+#define ORB_SLAM3_ROS_SRC_ODOMETRY_PUBLISHER_SRC_ODOMETRY_PUBLISHER_H_
+
+#include <ros/ros.h>
+
+namespace orb_slam3 {
+
+class OdometryPublisher {
+ public:
+  OdometryPublisher(ros::NodeHandle & n);
+ private:
+
+
+};
+
+}
+
+#endif //ORB_SLAM3_ROS_SRC_ODOMETRY_PUBLISHER_SRC_ODOMETRY_PUBLISHER_H_
