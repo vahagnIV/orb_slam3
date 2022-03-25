e3006c2d8e602879316ecb8f22b19d52f48f214a
vahagnIV <vahagn.yeghikyan@gmail.com>
2022-03-25 17:13:04 +0400

    Calculating

--- src/optimization/edges/se3_project_xyz_pose.cpp
+++ src/optimization/edges/se3_project_xyz_pose.cpp
@@ -10,14 +10,14 @@ namespace orb_slam3 {
 namespace optimization {
 namespace edges {
 
-SE3ProjectXYZPose::SE3ProjectXYZPose(const camera::MonocularCamera *camera, precision_t threshold)
+SE3ProjectXYZPose::SE3ProjectXYZPose(const camera::MonocularCamera * camera, precision_t threshold)
     : camera_(camera), threshold_(threshold), step_(0) {
   error()[0] = error()[1] = 0;
 }
 
 bool SE3ProjectXYZPose::IsDepthPositive() const {
-  auto point = dynamic_cast<g2o::VertexPointXYZ *>(_vertices[1]);
-  auto pose = dynamic_cast<g2o::VertexSE3Expmap *>(_vertices[0]);
+  auto point = dynamic_cast<vertices::MapPointVertex *>(_vertices[1]);
+  auto pose = dynamic_cast<vertices::FrameVertex *>(_vertices[0]);
   return pose->estimate().map(point->estimate()).z() > 1e-4;
 }
 
@@ -38,9 +38,9 @@ void SE3ProjectXYZPose::linearizeOplus() {
   auto point = dynamic_cast<vertices::MapPointVertex *>(_vertices[1]);
 
   g2o::Vector3 pt_camera_system = pose->estimate().map(point->estimate());
-  const double &x = pt_camera_system.x();
-  const double &y = pt_camera_system.y();
-  const double &z = pt_camera_system.z();
+  const double & x = pt_camera_system.x();
+  const double & y = pt_camera_system.y();
+  const double & z = pt_camera_system.z();
   if (z == 0) {
     std::cout << "Frame id " << pose->GetFrame()->Id() << std::endl;;
     TMatrix33 r = pose->estimate().rotation().toRotationMatrix();
@@ -59,6 +59,7 @@ void SE3ProjectXYZPose::linearizeOplus() {
   }
 
   camera::ProjectionJacobianType projection_jacobian;
+
   camera_->ComputeJacobian(pt_camera_system, projection_jacobian);
 
   Eigen::Matrix<double, 3, 6> se3_jacobian;
@@ -66,21 +67,25 @@ void SE3ProjectXYZPose::linearizeOplus() {
   se3_jacobian << 0.f, z, -y, 1.f, 0.f, 0.f,
       -z, 0.f, x, 0.f, 1.f, 0.f,
       y, -x, 0.f, 0.f, 0.f, 1.f;
+
   Eigen::Matrix<precision_t, 2, 6> Xi = projection_jacobian * se3_jacobian;;
   Eigen::Matrix<precision_t, 2, 3> Xj = projection_jacobian * pose->estimate().rotation().toRotationMatrix();
-  BABinaryEdge::linearizeOplus();
+  BaseFixedSizedEdge::linearizeOplus();
+//  std::cout << "Interesting:\n" << projection_jacobian.inverse() * _jacobianOplusXi << std::endl;
+  std::cout << point->GetMapPoint() << std::endl;
+  std::cout << pose->GetFrame()->Id() << std::endl;
   std::cout << _jacobianOplusXi << std::endl;
   std::cout << Xi << std::endl;
   Eigen::Matrix<precision_t, 2, 6> deltai = _jacobianOplusXi - Xi;
   Eigen::Matrix<precision_t, 2, 3> deltaj = _jacobianOplusXj - Xj;
   std::cout << deltai << std::endl;
   std::cout << deltaj << std::endl;
-  precision_t di = std::abs((deltai).sum()) ;
+  precision_t di = std::abs((deltai).sum());
   std::cout << "Di = " << di << std::endl;
-  if (di > 1e-2)
-    throw std::runtime_error("ppp");
-  if (std::abs((deltaj).sum()) > 1e-2)
-    throw std::runtime_error("ppp");
+//  if (di > 1e-2)
+//    throw std::runtime_error("ppp");
+//  if (std::abs((deltaj).sum()) > 1e-2)
+//    throw std::runtime_error("ppp");
 //  _jacobianOplusXi = projection_jacobian * se3_jacobian;
 //  _jacobianOplusXj = projection_jacobian * pose->estimate().rotation().toRotationMatrix();
 }
